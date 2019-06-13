e2b4acb649f497f7c0ec89edfd4b58a034bc0a86
C3NZ <cenz@cenz.io>
2019-06-12 18:57:08 -0700

    [feat] refactor for the simple linear regression

--- deepsale.py
+++ deepsale.py
@@ -7,14 +7,15 @@ from keras.layers.core import Activation, Dense, Dropout
 from keras.models import Sequential
 from keras.utils import np_utils
 from sklearn.datasets import load_boston
-from sklearn.metrics import r2_score
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 
 
 def get_train_test(bos_df: pd.DataFrame) -> tuple:
     """
-        Obtain the train test split 
+        Obtain the training and testing data.
     """
     X_data = []
     Y_data = []
@@ -28,6 +29,9 @@ def get_train_test(bos_df: pd.DataFrame) -> tuple:
 
 
 def get_bos_df() -> pd.DataFrame:
+    '''
+        Obtain the boston dataframe.
+    '''
     boston_data = load_boston()
     bos_df = pd.DataFrame(boston_data.data)
     bos_df.columns = boston_data.feature_names
@@ -36,7 +40,35 @@ def get_bos_df() -> pd.DataFrame:
     return bos_df
 
 
-def linear_regression():
+def simple_linear_regression():
+    '''
+        Create our simple linear regression model.
+    '''
+    bos_df = get_bos_df()
+    scaler = MinMaxScaler()
+
+    X_train, X_test, Y_train, Y_test = get_train_test(bos_df)
+
+    model = Sequential()
+    model.add(Dense(1, input_dim=13, activation="linear"))
+    model.compile(optimizer="adam", loss="mse", metrics=["mse", "mae"])
+    model.fit(
+        scaler.fit_transform(X_train), Y_train, epochs=200, batch_size=1, verbose=1
+    )
+
+    lr = LinearRegression()
+    lr.fit(scaler.fit_transform(X_train), Y_train)
+
+    loss, mse, mae = model.evaluate(scaler.fit_transform(X_test), Y_test, verbose=0)
+    sklearn_mse = 
+    print(f"MSE for Keras NN: {mse}")
+    print(f"MSE for LR model: {}")
+
+
+def complex_linear_regression():
+    '''
+        Create a complex linear regression model.
+    '''
 
     bos_df = get_bos_df()
     scaler = MinMaxScaler()
@@ -64,7 +96,10 @@ def linear_regression():
     print(f"r^2 = {r2_score(model.predict(scaler.fit_transform(X_test)), Y_test)}")
 
 
-def logistic_regression():
+def complex_logistic_regression():
+    '''
+        Create our complex logistic regression model.
+    '''
     dataframe = pd.read_csv("diabetes.csv")
     scaler = MinMaxScaler()
 
@@ -79,13 +114,13 @@ def logistic_regression():
     model.add(Dense(2))
     model.add(Activation("softmax"))
     model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
-    print(X_train)
+    print(np_utils.to_categorical(Y_train))
     model.fit(
         scaler.fit_transform(X_train),
         np_utils.to_categorical(Y_train),
         epochs=200,
         batch_size=1,
-        verbose=1,
+        verbose=0,
     )
     loss, accuracy = model.evaluate(
         scaler.fit_transform(X_test), np_utils.to_categorical(Y_test)
